66b6748c6aeab05a5ab2814b242956b60d2f04ae
seonghee <tjdgmll2@naver.com>
2019-03-22 08:24:34 +0000

    複合検索

--- app/controllers/realestates_controller.rb
+++ app/controllers/realestates_controller.rb
@@ -19,9 +19,19 @@ class RealestatesController < ApplicationController
     @product = current_office.products.build(product_params)
     # respond_to do |format|
       if @product.save
-        params[:images]['image'].each do |a|
-          @images = @product.images.create!(:image => a)
+        
+        if params[:images]
+          params[:images]['image'].each do |a| 
+            @images = @product.images.create!(:image => a)
+          end
         end
+        
+        if params[:product_terms]
+          params[:product_terms]['product_term'].each do |t| 
+            @product_terms = @product.product_terms.create!(:term_id => t)
+          end
+        end
+        
         # ContactMailer.picture_mail(current_user).deliver
         # address로 위도 경도 구하기 latitude/longitude
         
@@ -34,7 +44,27 @@ class RealestatesController < ApplicationController
   
   def search
     @product = Product.where("address like ?", "%#{params[:search_addr]}%")
-
+    
+    unless params[:search_rent].blank?
+      @product = @product.where("rent <= ?", "#{params[:search_rent]}")
+    end
+    
+    unless params[:search_structures].blank?
+      @product = @product.where(:structure_id => params[:search_structures][:search_structure])
+    end
+    
+    unless params[:search_types].blank?
+      @product = @product.where(:type_id => params[:search_types][:search_type])
+    end
+    
+    unless params[:search_terms].blank?
+      @product.each do |product|
+        @product_terms = product.product_terms.where(:term_id => params[:search_terms][:search_term])
+        # 조건검색 구현 어떻게 하쥬?
+      end
+      
+    end
+    
     render 'index'
   end
   
@@ -68,7 +98,7 @@ class RealestatesController < ApplicationController
   private
   
   def product_params
-    params.require(:product).permit(:name, :deposit, :reward, :area, :structure_id, :type_id, :address, :office_id, :m_fees, :rent, :floor, images_attributes: [:id, :product_id, :image]) 
+    params.require(:product).permit(:name, :deposit, :reward, :area, :structure_id, :type_id, :address, :office_id, :m_fees, :rent, :floor, images_attributes: [:id, :product_id, :image], product_terms_attriutes: [:id, :product_id, :term_id]) 
   end
   
   def set_product
